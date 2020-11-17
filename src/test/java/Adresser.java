849e0b54924d2feaf5de4a2773daff493b62d513
Markus <markusinge1@gmail.com>
2020-11-17 17:36:36 +0100

    La til en test i Adresser.java.

--- src/test/java/Adresser.java
+++ src/test/java/Adresser.java
@@ -1,8 +1,15 @@
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 
 public class Adresser {
     @Test()
     public void printmessage(){
         System.out.println("Hello");
+    }
+
+    @Test
+    public void leap_years_are_divisible_by_4_but_not_by_100() {
+        String actual = "Hello";
+
+        Assertions.assertEquals("Hello", actual);
     }
 }
