bdf7d0a745bd320576ce016d6843da00f6a0c3db
Markus <markusinge1@gmail.com>
2020-11-25 11:39:24 +0100

    Jobbet videre på GUI. Mye småendringer.

--- src/main/java/GUI/LoginFrame.java
+++ src/main/java/GUI/LoginFrame.java
@@ -9,6 +9,9 @@ public class LoginFrame extends JFrame implements ActionListener{
 
     //Global knapper
     JButton loginKnapp, registrerKnapp;
+    Color bakgrunnFarge1 = new Color(203,241,245);
+    Color bakgrunnFarge2 = new Color(166,227,233);
+    Color fontFarge1 = new Color(113,201,206);
 
     LoginFrame() {
 
@@ -18,7 +21,7 @@ public class LoginFrame extends JFrame implements ActionListener{
         this.setResizable(false); //Setter framen til å ikke være resizeable.
         this.setSize(1280, 800);   //Setter størrelsen på framen.
         this.setLocationRelativeTo(null);  //Åpner framen midt på skjermen.
-        this.getContentPane().setBackground(new Color(187,222,214));   //Setter bakgrunnsfargen.
+        this.getContentPane().setBackground(bakgrunnFarge1);   //Setter bakgrunnsfargen.
 
         //Lage tekstfelt.
         JTextField brukernavnFelt = new JTextField();
@@ -34,16 +37,16 @@ public class LoginFrame extends JFrame implements ActionListener{
         loginKnapp.setFocusable(false);
         loginKnapp.addActionListener(this);
         loginKnapp.setFont(new Font("Helvetica", Font.BOLD, 18));
-        loginKnapp.setBackground(new Color(250,227,217));
-        loginKnapp.setForeground(new Color(255,182,185));
+        loginKnapp.setBackground(bakgrunnFarge2);
+        loginKnapp.setForeground(fontFarge1);
         loginKnapp.setBounds(490, 260, 145, 50);
 
         registrerKnapp = new JButton("Registrer");    //Knapp for å registrere ny bruker
         registrerKnapp.setFocusable(false);
         registrerKnapp.addActionListener(e -> System.out.println("Send til registrerings skjerm."));
         registrerKnapp.setFont(new Font("Helvetica", Font.BOLD, 18));
-        registrerKnapp.setBackground(new Color(250,227,217));
-        registrerKnapp.setForeground(new Color(255,182,185));
+        registrerKnapp.setBackground(bakgrunnFarge2);
+        registrerKnapp.setForeground(fontFarge1);
         registrerKnapp.setBounds(645, 260, 145, 50);
 
         //Lage Ikon til applikasjonen. (fant ingen bra ikoner så ga opp på det :c)
@@ -52,22 +55,22 @@ public class LoginFrame extends JFrame implements ActionListener{
 
         //Paneler til velkomsts/login skjerm.
         JPanel velkomstTittelPanel = new JPanel();
-        velkomstTittelPanel.setBackground(new Color(187,222,214));
+        velkomstTittelPanel.setBackground(bakgrunnFarge1);
         velkomstTittelPanel.setBounds(0,0,1280,100);
         velkomstTittelPanel.setLayout(new BorderLayout());
 
         JPanel loginRegisterPanel = new JPanel();
-        loginRegisterPanel.setBackground(new Color(187,222,214));
+        loginRegisterPanel.setBackground(bakgrunnFarge1);
         loginRegisterPanel.setBounds(0,100,1280,600);
 
         JPanel footnote = new JPanel();
-        footnote.setBackground(new Color(187,222,214));
+        footnote.setBackground(bakgrunnFarge1);
         footnote.setBounds(0,700,1280,62);
 
         //Lage noen labels.
         JLabel label1 = new JLabel();
         label1.setText("Park & Disembark");
-        label1.setForeground(new Color (97,192,191));
+        label1.setForeground(fontFarge1);
         label1.setFont(new Font("Sans-Serif", Font.BOLD, 60));
         label1.setVerticalAlignment(JLabel.BOTTOM);
         label1.setHorizontalAlignment(JLabel.CENTER);
--- src/main/java/GUI/MainFrame.java
+++ src/main/java/GUI/MainFrame.java
@@ -8,6 +8,10 @@ import java.awt.event.ActionListener;
 public class MainFrame extends JFrame implements ActionListener {
 
     JButton leieKnapp, utleieKnapp, profilKnapp, instillingerKnapp;
+    JPanel leiePanel, utleiePanel, profilPanel, instillingerPanel, mainPanel;
+    Color bakgrunnFarge1 = new Color(203,241,245);
+    Color bakgrunnFarge2 = new Color(166,227,233);
+    Color fontFarge1 = new Color(113,201,206);
 
     MainFrame() {
 
@@ -17,10 +21,13 @@ public class MainFrame extends JFrame implements ActionListener {
         this.setResizable(false); //Setter framen til å ikke være resizeable.
         this.setSize(1280, 800);   //Setter størrelsen på framen.
         this.setLocationRelativeTo(null);  //Åpner framen midt på skjermen.
-        this.getContentPane().setBackground(new Color(187,222,214));   //Setter bakgrunnsfargen.
+        this.getContentPane().setBackground(bakgrunnFarge1);   //Setter bakgrunnsfargen.
         this.setLayout(null);
 
         //Lage tekstfelt.
+        JTextField sokeFeltLeie = new JTextField();
+        sokeFeltLeie.setBounds(490, 120, 300,50);
+        sokeFeltLeie.setFont(new Font("Helvetica", Font.PLAIN, 22));
 
 
         //Lage knapper.
@@ -28,32 +35,32 @@ public class MainFrame extends JFrame implements ActionListener {
         leieKnapp.setFocusable(false);
         leieKnapp.addActionListener(this);
         leieKnapp.setFont(new Font("Helvetica", Font.BOLD, 28));
-        leieKnapp.setBackground(new Color(250,227,217));
-        leieKnapp.setForeground(new Color(255,182,185));
+        leieKnapp.setBackground(bakgrunnFarge2);
+        leieKnapp.setForeground(fontFarge1);
         leieKnapp.setBorder(null);
 
         utleieKnapp = new JButton("Utleie");   //Knapp for å leie.
         utleieKnapp.setFocusable(false);
         utleieKnapp.addActionListener(this);
         utleieKnapp.setFont(new Font("Helvetica", Font.BOLD, 28));
-        utleieKnapp.setBackground(new Color(250,227,217));
-        utleieKnapp.setForeground(new Color(255,182,185));
+        utleieKnapp.setBackground(bakgrunnFarge2);
+        utleieKnapp.setForeground(fontFarge1);
         utleieKnapp.setBorder(null);
 
         profilKnapp = new JButton("Profil");   //Knapp for å leie.
         profilKnapp.setFocusable(false);
         profilKnapp.addActionListener(this);
         profilKnapp.setFont(new Font("Helvetica", Font.BOLD, 28));
-        profilKnapp.setBackground(new Color(250,227,217));
-        profilKnapp.setForeground(new Color(255,182,185));
+        profilKnapp.setBackground(bakgrunnFarge2);
+        profilKnapp.setForeground(fontFarge1);
         profilKnapp.setBorder(null);
 
         instillingerKnapp = new JButton("Instillinger");   //Knapp for å leie.
         instillingerKnapp.setFocusable(false);
         instillingerKnapp.addActionListener(this);
         instillingerKnapp.setFont(new Font("Helvetica", Font.BOLD, 28));
-        instillingerKnapp.setBackground(new Color(250,227,217));
-        instillingerKnapp.setForeground(new Color(255,182,185));
+        instillingerKnapp.setBackground(bakgrunnFarge2);
+        instillingerKnapp.setForeground(fontFarge1);
         instillingerKnapp.setBorder(null);
 
         //Lage Ikon til applikasjonen. (fant ingen bra ikoner så ga opp på det :c)
@@ -62,28 +69,54 @@ public class MainFrame extends JFrame implements ActionListener {
 
         //Paneler til velkomsts/login skjerm.
         JPanel tittelPanel = new JPanel();
-        tittelPanel.setBackground(new Color(187,222,214));
+        tittelPanel.setBackground(bakgrunnFarge1);
         tittelPanel.setBounds(0,0,1280,100);
         tittelPanel.setLayout(new BorderLayout());
 
-        JPanel mainPanel = new JPanel();
-        mainPanel.setBackground(new Color(187,222,214));
+        mainPanel = new JPanel();
+        mainPanel.setBackground(bakgrunnFarge1);
         mainPanel.setBounds(0,100,1280,550);
 
         JPanel footnote = new JPanel();
-        footnote.setBackground(new Color(250,227,217));
+        footnote.setBackground(bakgrunnFarge1);
         footnote.setBounds(0,650,1280,120);
 
+        //Panel for de forskjellige knappene.
+        leiePanel = new JPanel();
+        leiePanel.setBackground(bakgrunnFarge1);
+        leiePanel.setBounds(0,100,1280,550);
+
+        utleiePanel = new JPanel();
+        utleiePanel.setBackground(new Color(129,219,167));
+        utleiePanel.setBounds(0,100,1280,550);
+
+        profilPanel = new JPanel();
+        profilPanel.setBackground(new Color(117,145,223));
+        profilPanel.setBounds(0,100,1280,550);
+
+        instillingerPanel = new JPanel();
+        instillingerPanel.setBackground(new Color(223,203,85));
+        instillingerPanel.setBounds(0,100,1280,550);
+
         //Lage noen labels.
-        JLabel label1 = new JLabel();
-        label1.setText("Park & Disembark");
-        label1.setForeground(new Color (97,192,191));
-        label1.setFont(new Font("Sans-Serif", Font.BOLD, 60));
-        label1.setVerticalAlignment(JLabel.BOTTOM);
-        label1.setHorizontalAlignment(JLabel.CENTER);
+        JLabel tittelLabel = new JLabel();
+        tittelLabel.setText("Park & Disembark");
+        tittelLabel.setForeground(fontFarge1);
+        tittelLabel.setFont(new Font("Sans-Serif", Font.BOLD, 60));
+        tittelLabel.setVerticalAlignment(JLabel.BOTTOM);
+        tittelLabel.setHorizontalAlignment(JLabel.CENTER);
+
+        JLabel mainPanelBackgroundLabel = new JLabel();
+        ImageIcon mainPanelBackground = new ImageIcon("src/imgs/backgroundthingy2.jpg");
+        mainPanelBackgroundLabel.setIcon(mainPanelBackground);
 
         //Legger til labels og knapper til panels og endre layout.
-        tittelPanel.add(label1);
+        tittelPanel.add(tittelLabel);
+
+        mainPanel.add(mainPanelBackgroundLabel);
+
+
+
         footnote.add(leieKnapp);
         footnote.add(utleieKnapp);
         footnote.add(profilKnapp);
@@ -94,23 +127,55 @@ public class MainFrame extends JFrame implements ActionListener {
         this.add(tittelPanel);
         this.add(mainPanel);
         this.add(footnote);
+        this.add(leiePanel);
+        this.add(utleiePanel);
+        this.add(profilPanel);
+        this.add(instillingerPanel);
 
         //Gjør GUI synelig.
+        leiePanel.setVisible(false);
+        utleiePanel.setVisible(false);
+        profilPanel.setVisible(false);
+        instillingerPanel.setVisible(false);
         this.setVisible(true);
     }
 
     @Override
     public void actionPerformed(ActionEvent e) {
         if(e.getSource()==leieKnapp) {
-            System.out.println("leie!");
+            mainPanel.setVisible(false);
+            leiePanel.setVisible(true);
+            utleiePanel.setVisible(false);
+            profilPanel.setVisible(false);
+            instillingerPanel.setVisible(false);
+
+            System.out.println("login.");
         }
         if(e.getSource()==utleieKnapp) {
+            mainPanel.setVisible(false);
+            leiePanel.setVisible(false);
+            utleiePanel.setVisible(true);
+            profilPanel.setVisible(false);
+            instillingerPanel.setVisible(false);
+
             System.out.println("utleie!");
         }
         if(e.getSource()==profilKnapp) {
+            mainPanel.setVisible(false);
+            leiePanel.setVisible(false);
+            utleiePanel.setVisible(false);
+            profilPanel.setVisible(true);
+            instillingerPanel.setVisible(false);
+
             System.out.println("profil!");
         }
         if(e.getSource()==instillingerKnapp) {
+            mainPanel.setVisible(false);
+            leiePanel.setVisible(false);
+            utleiePanel.setVisible(false);
+            profilPanel.setVisible(false);
+            instillingerPanel.setVisible(true);
+
             System.out.println("innstillinger!");
         }
     }
